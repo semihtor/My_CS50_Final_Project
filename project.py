086325cbab94bc9cc11bf1a7119c21fcef1cb866
Semih Tor <82044766+semihtor@users.noreply.github.com>
2024-01-11 15:17:31 +0300

    Update project.py
    Implemented functions to compare code to answer, to check answer for game compatibility and to compare answer to created code

--- project.py
+++ project.py
@@ -2,10 +2,20 @@ import random
 import emoji
 from tabulate import tabulate
 
-# duplicates_allowed will be 1 or 0
+class AnswerNotAllDigitsException(Exception):
+    "Raised when the input value is not all digits"
+    pass
+
+class AnswerNotCompatibleWithDifficultyException(Exception):
+    "Raised when the input value is not compatible with difficulty"
+    pass
+class NoCheatCodeError(Exception):
+    "Raised when the input value is Konami cheat code"
+    pass
+
 
 def generate_code(difficulty, duplicates_allowed):
-
+    # duplicates_allowed will be 1 or 0
     code = ""
 
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -19,7 +29,6 @@ def generate_code(difficulty, duplicates_allowed):
         for x in range(0, difficulty):
             code += str(numbers[x])
     return code
-
 
 def create_game_board(difficulty):
     
@@ -43,5 +52,55 @@ def create_game_board(difficulty):
     for x in range(0, round_count[difficulty]):
         rounds_table.insert(x, rounds_placeholder)
 
+    # print(rounds_table)
+    
     print(tabulate(rounds_table, headers, tablefmt = "grid", colalign=("center", "center")))
 
+def compare_answer_to_generated_code(code, answer, difficulty):
+    #code answer will be 2,3,4 or 5 digit numbers turned to strings
+    correct_digit = 0
+
+    correct_placement = 0
+    
+    x = 0
+
+    for x in range(0, difficulty):
+        if answer[x] == code[x]:
+            correct_placement += 1
+        elif answer[x] in code:
+            correct_digit += 1
+    
+    return correct_placement, correct_digit
+    
+def check_user_guess_for_game_compatibility(difficulty):
+    
+    while True:
+        
+        try:
+            user_guess = input("Please enter your guess: ")
+            if user_guess == "up up down down left right left right b a start" or user_guess == "up up down down left right left right b a select":
+                raise NoCheatCodeError
+            else:
+                user_guess = user_guess.replace(" ", "")
+            
+            if not user_guess.isdigit():
+                raise AnswerNotAllDigitsException
+            elif len(user_guess) != difficulty:
+                raise AnswerNotCompatibleWithDifficultyException
+            else:
+                return user_guess
+        
+        except AnswerNotCompatibleWithDifficultyException:
+            print(f"Please enter an answer containing {difficulty} digits")
+
+        except AnswerNotAllDigitsException:
+            print("Please enter an answer containing only digits")
+        
+        except NoCheatCodeError:
+            print(emoji.emojize("Sorry, this game doesn't support any cheats :disappointed_face:"))
+
+def main():
+    check_user_guess_for_game_compatibility(4)
+
+if __name__ == "__main__":
+    main()
